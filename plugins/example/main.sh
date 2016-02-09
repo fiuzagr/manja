98a6c3eb36f26035e3ee3fcee5580c022ecf61d8
fiuzagr <gui.fiuza@gmail.com>
2016-02-09 17:50:10 -0200

    Update...

--- plugins/example/main.sh
+++ plugins/example/main.sh
@@ -4,22 +4,24 @@ manja_example_plugin() {
   echo
   echo "$(fgcolor green)$(icon check)$(fgcolor end) $(bgcolor blue)Example of use$(bgcolor end)"
   echo
-  echo 'You can calling manja functions:'
+  echo 'You can call manja functions:'
   echo "$(a bold OS:) $(a invert)$(manja os)$(a end)"
   echo
   echo 
   echo 'And can use SHML vendor plugin to style your code'
-  echo 
+  echo 'with colors (fg and bg), attributes (bold, underline, invert),'
+  echo 'elements (hr, br, indent, tab), icons, emojis and color bar'
+  echo
   echo 'Emojis:'
   echo "$(fgcolor red)$(emoji fire)$(fgcolor end)$(i)and$(i)$(fgcolor yellow)$(emoji monkey)$(fgcolor end)"
   echo 'and some others'
   echo
   echo 'Elements:'
   echo "$(hr '--' 10)"
-  echo "$(i)You can indent lines and make $(a bold bold) text"
-  echo "Can too $(a underline underline) and $(a invert invert) text"
+  echo "$(i)You can make indent lines and make $(a bold bold) text"
+  echo "You can make too $(a underline underline) and $(a invert invert) text"
   echo "And line$(br)break"
-  echo "$(tab) and tabulated"
+  echo "$(tab)and tabulated lines"
   echo 
 }
 manja_example_plugin $@
